b7613b2bcd13369da1dde1c457c5a361e9629bcf
Nozomu Sudo <nsadnote22@gmail.com>
2024-02-29 12:17:06 +0900

    home?

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
 
     <div style={HomeStyle.container}>
       <div style={{ position: 'relative', width:"100%", height:"100%", marginTop:'5%'}}>
-        <Wood3D />
+        {/*<Wood3D />*/}
       </div>
 
       <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', zIndex: 1 }}>
